fbe8f00cb65b78cd45c89fdf362bc30cfb9f7bad
Care <careluanne@gmail.com>
2021-04-16 10:27:56 -0400

    Return Number of menu rows based on input [Pizza]

--- TDDPratice3Tests/TDDPratice3Tests.swift
+++ TDDPratice3Tests/TDDPratice3Tests.swift
@@ -8,26 +8,25 @@
 import XCTest
 @testable import TDDPratice3
 
+struct MenuDataSource {
+    let pizzas: [Pizza]
+    let numberOfSections = 1
+    func numberOfRows(inSection section: Int) -> Int {
+        return pizzas.count
+    }
+}
+
+struct Pizza {
+    
+}
+
 class TDDPratice3Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testHasOneSection() {
+        let dataSource = MenuDataSource(pizzas: [Pizza()])
+        XCTAssertEqual(dataSource.numberOfSections, 1)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testRowsInSection() {
+        let dataSource = MenuDataSource(pizzas: [Pizza(), Pizza(), Pizza()])
+        XCTAssertEqual(dataSource.numberOfRows(inSection: 0), 3)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
